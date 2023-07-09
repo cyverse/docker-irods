3efdb381143735b131384d8b25d63d9b26bce156
Tony Edgin <tedgin@cyverse.org>
2023-07-09 13:28:11 -0700

    Indent test peripheral messages

--- test/control-dbms.sh
+++ test/control-dbms.sh
@@ -15,23 +15,35 @@ main() {
 	esac
 }
 
-
 start_dbms() {
 	printf 'Starting PostgreSQL\n'
 
 	sudo --user postgres \
-		/usr/lib/postgresql/10/bin/pg_ctl \
-			--log=/var/log/postgresql/postgresql-10-main.log \
-			--options='--config_file=/etc/postgresql/10/main/postgresql.conf' \
-			--pgdata=/var/lib/postgresql/10/main \
-			start
+			/usr/lib/postgresql/10/bin/pg_ctl \
+				--log=/var/log/postgresql/postgresql-10-main.log \
+				--options='--config_file=/etc/postgresql/10/main/postgresql.conf' \
+				--pgdata=/var/lib/postgresql/10/main \
+				start \
+		> >(indent /dev/stdout) \
+		2> >(indent /dev/stderr)
 }
-
 
 stop_dbms() {
 	printf 'Stopping PostgreSQL\n'
-	sudo --user postgres /usr/lib/postgresql/10/bin/pg_ctl --pgdata=/var/lib/postgresql/10/main stop
+
+	sudo --user postgres \
+			/usr/lib/postgresql/10/bin/pg_ctl --pgdata=/var/lib/postgresql/10/main stop \
+		> >(indent /dev/stdout) \
+		2> >(indent /dev/stderr)
 }
 
+indent() {
+	local out="$1"
+
+	local line
+	while read -r line; do
+		printf '\t%s\n' "$line"
+	done > "$out"
+}
 
 main "$@"
